f96ff703af4c66ef5ca41b61fbfb29cf15afa915
aimonimran <aimon.imran@gmail.com>
2022-09-13 17:53:38 +0500

    Implemented routes

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const cors=require("cors");
+
+const corsOptions = {
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
 
 dotenv.config();
 
@@ -13,8 +26,12 @@ mongoose
     .catch(err => console.log(err));
 
 app.use(express.json());
+app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
+app.use('/api/products', productRoute);
+app.use('/api/carts', cartRoute);
+app.use('/api/orders', orderRoute);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log(`Backend server is running on port ${process.env.PORT || 5000}`);
-})
+});
